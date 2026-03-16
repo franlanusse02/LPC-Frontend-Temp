2c3366eff63e4543492d5d222cfeaa8fe3b5de7b
Mac Fran <franlanusse02@gmail.com>
2026-03-16 09:54:08 -0300

    Fixed logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css"
 export const metadata = {
   title: "P_COMEDORES",
   icons: {
-    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90' font-family='Helvetica Neue, Helvetica, Arial, sans-serif'>LPC</text></svg>",
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><circle cx='50' cy='50' r='50' fill='white'/><text x='50' y='64' font-size='38' font-family='Helvetica Neue, Helvetica, Arial, sans-serif' font-weight='bold' text-anchor='middle' fill='black'>LPC</text></svg>",
   },
 }
 
